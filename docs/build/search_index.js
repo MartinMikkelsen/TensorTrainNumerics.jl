33979651526378d8d256979bf99a2e8401d2011f
MartinMikkelsen <MartinMikkelsen@protonmail.com>
2024-12-17 13:39:24 +0100

    deploy documentation

---
+++ docs/build/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#TensorTrainNumerics.jl","page":"TensorTrainNumerics.jl","title":"TensorTrainNumerics.jl","text":"","category":"section"},{"location":"","page":"TensorTrainNumerics.jl","title":"TensorTrainNumerics.jl","text":"Documentation for TensorTrainNumerics.jl","category":"page"}]
+}
