ca04c6a6ae2572a977107b16a1ab15ff182fb6cd
MartinMikkelsen <MartinMikkelsen@protonmail.com>
2024-12-17 14:13:35 +0100

    init docs

--- docs/build/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"#TensorTrainNumerics.jl","page":"TensorTrainNumerics.jl","title":"TensorTrainNumerics.jl","text":"","category":"section"},{"location":"","page":"TensorTrainNumerics.jl","title":"TensorTrainNumerics.jl","text":"Documentation for TensorTrainNumerics.jl","category":"page"}]
-}
--- test/runtests.jl
+++
@@ -1,8 +0,0 @@
-using Test
-using TensorTrainNumerics
-
-include("test_interpolations.jl")
-include("test_tt_tools.jl")
-include("test_als.jl")
-include("test_mals.jl")
-include("test_dmrg.jl")
