234cd2f5301f1a4f067ea33d60ded85765b06258
MartinMikkelsen <MartinMikkelsen@protonmail.com>
2024-12-18 14:03:07 +0100

    updated CI

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,9 @@
 using Documenter, TensorTrainNumerics
 
-
 makedocs(
-    sitename = "TensorTrainNumerics",
+    sitename = "TensorTrainNumerics"
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
 deploydocs(
-    repo = "github.com/MartinMikkelsen/TensorTrainNumerics.jl.git",
-    target = "build",
-    branch="gh-pages",
+    repo = "github.com/MartinMikkelsen/TensorTrainNumerics.jl.git"
 )
