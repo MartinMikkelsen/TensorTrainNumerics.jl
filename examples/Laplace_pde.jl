f97fc5e70d304de1fca5a38e1b4b308f1a44cf67
MartinMikkelsen <MartinMikkelsen@protonmail.com>
2025-06-03 17:12:13 +0200

    updated MALS

--- examples/Laplace_pde.jl
+++ examples/Laplace_pde.jl
@@ -1,14 +1,11 @@
 using TensorTrainNumerics
 using CairoMakie
 
-cores = 10
-a = 0.0
-b = 1.0
+cores = 8
 
-xes = collect(range(a,b,length=2^cores))
-yes = collect(range(a,b,length=2^cores))
+xes = collect(range(0.0,1.0,length=2^cores))
 
-h = (b-a)/(2^cores)
+h = 1/(2^cores)
 p = 1.0
 s = 0.0
 v = 0.0
@@ -23,13 +20,19 @@ b = qtt_cos(cores) ⊗ qtt_basis_vector(cores, 1) + qtt_sin(cores) ⊗ qtt_basis
 
 initial_guess = rand_tt(b.ttv_dims, b.ttv_rks)
 
-x_dmrg = dmrg_linsolve(A, b, initial_guess; sweep_count=50,tol=1e-15)
-solution = reshape(qtt_to_function(x_dmrg), 2^cores, 2^cores)
+x_mals = mals_linsolv(A, b, initial_guess)
+x_dmrg = dmrg_linsolve(A, b, initial_guess)
+x_als = als_linsolv(A, b, initial_guess;
+                     sweep_count = 2,
+                     it_solver   = true,
+                     r_itsolver  = 5000)
+
+solution = reshape(qtt_to_function(x_mals), 2^cores, 2^cores)
 
 fig = Figure()
 cmap = :roma
 ax = Axis(fig[1, 1], title = "Laplace Solution", xlabel = "x", ylabel = "y")
-hm = heatmap!(ax, xes, yes, solution; colormap = cmap)
+hm = heatmap!(ax, xes, xes, solution; colormap = cmap)
 Colorbar(fig[1, 2], hm, label = "u(x, y)")
 fig
 
