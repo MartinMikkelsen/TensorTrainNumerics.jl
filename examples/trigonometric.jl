d524dd58044ac418206244ae00d10b6512b9f786
MartinMikkelsen <MartinMikkelsen@protonmail.com>
2025-07-30 13:47:27 +0200

    updated Hadamard product

--- examples/trigonometric.jl
+++ examples/trigonometric.jl
@@ -8,7 +8,6 @@ A2 = qtt_sin(d, λ = π)
 A3 = qtt_cos(d, λ = π)
 A4 = qtt_polynom([0.0, 2.0, 3.0, -8.0, -5.0], d; a = 0.0, b = 1.0)
 
-
 qtt_values_exponential = qtt_to_function(A1)
 qtt_values_sin = qtt_to_function(A2)
 qtt_values_cos = qtt_to_function(A3)
@@ -45,6 +44,29 @@ let
 
     lines!(ax4, x_points, original_values_polynom, label = "Sine function")
     lines!(ax4, x_points, qtt_values_polynom, label = "QTT sine function", linestyle = :dash, color = :red)
+
+    fig
+end
+
+
+let
+    fig = Figure()
+    ax1 = Axis(fig[2, 2], xlabel = "x", ylabel = "f(x)")
+    ax2 = Axis(fig[1, 1], xlabel = "x", ylabel = "f(x)")
+    ax3 = Axis(fig[1, 2], xlabel = "x", ylabel = "f(x)")
+    ax4 = Axis(fig[2, 1], xlabel = "x", ylabel = "f(x)")
+
+    lines!(ax1, x_points, cos.(π^2 * x_points) .* sin.(π^2 * x_points))
+    lines!(ax1, x_points, qtt_to_function(A2 ⊕ A3), linestyle = :dash, color = :green)
+
+    lines!(ax2, x_points, exp.(x_points) .* sin.(π^2 * x_points))
+    lines!(ax2, x_points, qtt_to_function(A1 ⊕ A2), linestyle = :dash, color = :green)
+
+    lines!(ax3, x_points, values_polynom.(x_points) .* sin.(π^2 * x_points))
+    lines!(ax3, x_points, qtt_to_function(A4 ⊕ A2), linestyle = :dash, color = :green)
+
+    lines!(ax4, x_points, values_polynom.(x_points) .* cos.(π^2 * x_points))
+    lines!(ax4, x_points, qtt_to_function(A4 ⊕ A3), linestyle = :dash, color = :green)
 
     fig
 end
