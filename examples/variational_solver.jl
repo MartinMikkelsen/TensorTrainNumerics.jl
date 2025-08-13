e9c96a96e2156a3d18caa51aaa3216a7d86ea7b7
MartinMikkelsen <MartinMikkelsen@protonmail.com>
2025-08-13 16:41:14 +0200

    added gradient descent example and fixed extensions

---
+++ examples/variational_solver.jl
@@ -0,0 +1,37 @@
+using TensorTrainNumerics
+using OptimKit
+using KrylovKit
+
+d = 6
+N = 2^d
+h = 1 / (N - 1)
+Δ = toeplitz_to_qtto(2.0, -1.0, -1.0, d)
+kappa = 0.1
+A = kappa * Δ
+
+f = qtt_sin(d, λ = π)
+
+function fg(u::TTvector)
+    û  = orthogonalize(u)           
+    Au = A*û                       
+    val = 0.5 * real(dot(û, Au)) - real(dot(f, û))
+    grad = Au - f
+    return val, grad
+end
+
+x0 = rand_tt(f.ttv_dims, f.ttv_rks)
+
+method = GradientDescent()
+x, fx, gx, numfg, normgradhistor = optimize(fg, x0, method)
+
+relres = norm(A * x - f) / max(norm(f), eps())
+println("relative residual = ", relres)
+
+x_mals = mals_linsolve(A, f, x0)
+relres = norm(A * x_mals - f) / max(norm(f), eps())
+println("relative residual = ", relres)
+
+x_krylov, info = linsolve(A, f, x0)
+relres = norm(A * x_krylov - f) / max(norm(f), eps())
+println("relative residual = ", relres)
+
--- ext/TensorTrainNumericsVectorInterfaceExt/TensorTrainNumericsVectorInterfaceExt.jl
+++ ext/TensorTrainNumericsVectorInterfaceExt/TensorTrainNumericsVectorInterfaceExt.jl
@@ -2,115 +2,90 @@ module TensorTrainNumericsVectorInterfaceExt
 using TensorTrainNumerics
 using VectorInterface
 import Base: zero, copy
-import LinearAlgebra: dot, norm
-
 
 function VectorInterface.add(a::TTvector, b::TTvector)
-    return orthogonalize(a + b)
+    orthogonalize(a + b)
 end
 function VectorInterface.add(a::TToperator, b::TToperator)
-    return orthogonalize(a + b)
+    orthogonalize(a + b)
 end
+
 function VectorInterface.add(a::TTvector, b::TTvector, α::Number)
-    return orthogonalize(a + b * α)
+    orthogonalize(a + α*b)
 end
 function VectorInterface.add(a::TTvector, b::TTvector, α::Number, β::Number)
-    return orthogonalize(β * a + α * b)
+    orthogonalize(β*a + α*b)
 end
 
 function VectorInterface.add!(y::TTvector, x::TTvector)
-    result = y + x
-    y.ttv_vec = result.ttv_vec
-    y.ttv_rks = result.ttv_rks
-    y.ttv_dims = result.ttv_dims
-    y.ttv_ot = result.ttv_ot
-    return orthogonalize(y)
+    r = y + x
+    y.ttv_vec = r.ttv_vec; y.ttv_rks = r.ttv_rks; y.ttv_dims = r.ttv_dims; y.ttv_ot = r.ttv_ot
+    orthogonalize(y)
+end
+function VectorInterface.add!(y::TTvector{T,N}, x::TTvector{T,N}, α::Number, β::Number) where {T,N}
+    αT = convert(T, α); βT = convert(T, β)
+    r = βT*y + αT*x
+    y.ttv_vec = r.ttv_vec; y.ttv_rks = r.ttv_rks; y.ttv_dims = r.ttv_dims; y.ttv_ot = r.ttv_ot
+    orthogonalize(y)
 end
 
-function VectorInterface.add!(y::TTvector{T, N}, x::TTvector{T, N}, α::Number, β::Number) where {T, N}
-    αT = convert(T, α)
-    βT = convert(T, β)
-    result = αT * y + βT * x
-    y.ttv_vec = result.ttv_vec
-    y.ttv_rks = result.ttv_rks
-    y.ttv_dims = result.ttv_dims
-    y.ttv_ot = result.ttv_ot
-    return orthogonalize(y)
-end
-
-
-function VectorInterface.add!!(y::TTvector, x::TTvector, α::Number, β::Number)
-    if VectorInterface.promote_add(y, x, α, β) <: scalartype(y)
-        return VectorInterface.add!(y, x, α, β)
+function VectorInterface.add!!(y::TTvector, x::TTvector)
+    if promote_type(eltype(y), eltype(x)) <: eltype(y)
+        VectorInterface.add!(y, x)
     else
-        return orthogonalize(α * y + β * x)
+        orthogonalize(y + x)
+    end
+end
+function VectorInterface.add!!(y::TTvector, x::TTvector, α::Number, β::Number)
+    if promote_type(eltype(y), eltype(x), typeof(α), typeof(β)) <: eltype(y)
+        VectorInterface.add!(y, x, α, β)
+    else
+        r = orthogonalize(β*y + α*x)
+        y.ttv_vec = r.ttv_vec; y.ttv_rks = r.ttv_rks; y.ttv_dims = r.ttv_dims; y.ttv_ot = r.ttv_ot
+        y
     end
 end
 
 function VectorInterface.scale(x::TTvector, α::Number)
-    return orthogonalize(α * x)
+    orthogonalize(α * x)
 end
-
-function VectorInterface.scale!(x::TTvector, α::Number)
-    i = findfirst(isequal(0), x.ttv_ot)
-    x.ttv_vec[i] .= α .* x.ttv_vec[i]
-    return x
+function VectorInterface.scale!(x::TTvector{T}, α::Number) where {T}
+    αT = convert(T, α)
+    i = findfirst(==(0), x.ttv_ot); i === nothing && (i = 1)
+    @. x.ttv_vec[i] = αT * x.ttv_vec[i]
+    x
 end
-
-function VectorInterface.scale!!(x::TTvector{T, N}, α::Number) where {T, N}
-    T2 = typejoin(T, typeof(α))
-    return if T2 === T
-        orthogonalize(VectorInterface.scale!(x, α))
-    else
-        orthogonalize(VectorInterface.scale(x, α))
-    end
+function VectorInterface.scale!!(x::TTvector{T}, α::Number) where {T}
+    S = promote_type(T, typeof(α))
+    S === T ? orthogonalize(VectorInterface.scale!(x, α)) : orthogonalize(VectorInterface.scale(x, α))
 end
-
 function VectorInterface.scale!!(y::TTvector, x::TTvector, α::Number)
-    result = VectorInterface.scale(x, α)
-    y.ttv_vec = result.ttv_vec
-    y.ttv_rks = result.ttv_rks
-    y.ttv_dims = result.ttv_dims
-    y.ttv_ot = result.ttv_ot
-    return orthogonalize(y)
+    r = VectorInterface.scale(x, α)
+    y.ttv_vec = r.ttv_vec; y.ttv_rks = r.ttv_rks; y.ttv_dims = r.ttv_dims; y.ttv_ot = r.ttv_ot
+    orthogonalize(y)
 end
 
 function VectorInterface.zerovector(a::TTvector)
-    T = eltype(a)
-    return zeros_tt(T, a.ttv_dims, a.ttv_rks)
+    zeros_tt(eltype(a), a.ttv_dims, a.ttv_rks)
 end
-
 function VectorInterface.zerovector(a::TToperator)
-    return zeros_tt(eltype(a), a.tto_dims, a.tto_rks)
+    zeros_tt(eltype(a), a.tto_dims, a.tto_rks)
 end
 
-
-VectorInterface.length(a::TTvector) = prod(a.ttv_dims)
-
+VectorInterface.length(a::TTvector)   = prod(a.ttv_dims)
 VectorInterface.length(a::TToperator) = prod(a.tto_dims)
 
 zero(a::TTvector) = zeros_tt(eltype(a), a.ttv_dims, a.ttv_rks)
 
 function VectorInterface.inner(a::TTvector, b::TTvector)
-    return TensorTrainNumerics.dot(a, b)
+    real(TensorTrainNumerics.dot(a, b))
 end
 
-function VectorInterface.scalartype(a::TTvector)
-    return eltype(a)
-end
-
-function VectorInterface.scalartype(a::TToperator)
-    return eltype(a)
-end
+VectorInterface.scalartype(a::TTvector)   = eltype(a)
+VectorInterface.scalartype(a::TToperator) = eltype(a)
 
 function Base.copy(x::TTvector)
-    return TTvector(
-        x.N,
-        copy.(x.ttv_vec),
-        copy(x.ttv_dims),
-        copy(x.ttv_rks),
-        copy(x.ttv_ot),
-    )
+    TTvector(x.N, copy.(x.ttv_vec), copy(x.ttv_dims), copy(x.ttv_rks), copy(x.ttv_ot))
 end
 
 end
